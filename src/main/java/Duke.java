51273cd1fdf0c88572abe8a907019f876d942ada
wamikamalik <wamikamalik@hotmail.com>
2020-08-25 13:50:46 +0400

    Add mark as done
    Marks the task with a tick marker if done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -46,19 +46,46 @@ public class Duke {
                 else {
                     System.out.println("\n\tHere's your current To-Do list: ");
                     for (int i = 0; i<Task.getNoOfTasks(); i += 1) {
-                        System.out.println("\t\t"+(i+1)+". " + taskList[i].getName());
+                        System.out.println("\t\t"+ (i+1) +". [" + taskList[i].getIcon() + "] " + taskList[i].getName());
                     }
                     System.out.println();
                 }
 
+            }
+            else if(command.trim().startsWith("done")||command.trim().startsWith("Done")) {
+                try {
+                    String rawItemNo = command.trim().substring(5, command.length());
+                    if(rawItemNo.endsWith(".")) {
+                        rawItemNo = rawItemNo.substring(0,rawItemNo.length()-1);
+                    }
+                    int itemNo = Integer.parseInt(rawItemNo);
+                    //System.out.println("\n\t" + itemNo + "\n");
+                    if(itemNo==0 || itemNo>Task.getNoOfTasks()) {
+                        System.out.println("\n\tItem "+ itemNo +" does not exist.\n");
+                    }
+                    else if(taskList[itemNo-1].isDone()) {
+                        System.out.println("\n\tThis task was marked as done earlier!");
+                        System.out.println("\tTo see the list of incomplete tasks simply type \"list\".\n");
+                    }
+                    else {
+                        taskList[itemNo-1].markAsDone();
+                        System.out.println("\n\tGreat! I have updated your To-Do list for the following task:");
+                        System.out.println("\t\t ["+ taskList[itemNo-1].getIcon() + "] " + taskList[itemNo-1].getName()+"\n");
+                    }
+
+                }
+                catch (NumberFormatException error) {
+                    System.out.println("\n\tPlease enter a valid item number.\n");
+                }
             }
             else {
 
                 Task newItem = new Task(command);
                 taskList[Task.getNoOfTasks() - 1] = newItem;
                 System.out.println("\n\tAdded task \"" + newItem.getName() + "\" to your To-Do's!");
-                System.out.println("\tYou now have " + Task.getNoOfTasks() + " incomplete task"
-                        + (Task.getNoOfTasks() != 1 ? "s" : "") + ".\n");
+                System.out.println("\tYou now have " + Task.getIncompleteTasks() + " incomplete task"
+                        + (Task.getIncompleteTasks() != 1 ? "s" : "") + ".");
+                System.out.println("\tTo view the To-Do list simply type \"list\".\n");
                 
             }
             printLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,9 @@
 public class Task {
 
     private String name;
+    private boolean isDone;
     private static int noOfTasks = 0;
+    public static int incompleteTasks = 0;
 
     public Task() {
         this("");
@@ -9,14 +11,33 @@ public class Task {
 
     public Task(String name) {
         this.name = name;
+        this.isDone = false;
         noOfTasks += 1;
+        incompleteTasks+=1;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        incompleteTasks-=1;
+    }
+
     public static int getNoOfTasks() {
         return noOfTasks;
+    }
+
+    public static int getIncompleteTasks() {
+        return incompleteTasks;
+    }
+
+    public boolean isDone() {
+        return isDone;
     }
 }
