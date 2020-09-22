e22f5bb07e233a801c08dd5b37f5a5c100e77723
wamikamalik <wamikamalik@hotmail.com>
2020-09-22 17:52:17 +0400

    Update coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,6 @@ public class Duke {
     private TaskList tasks;
     private Ui ui;
     private FileManager file;
-    private Parser parser;
 
     public Duke() {
         ui = new Ui();
@@ -45,7 +44,7 @@ public class Duke {
             //the following code interprets the command entered by the user and takes appropriate actions.
             try {
                 String fullCommand = ui.getCommand();
-                parser = new Parser(fullCommand);
+                Parser parser = new Parser(fullCommand);
                 CommandType type = parser.extractType();
                 switch (type) {
                 case EXIT:
